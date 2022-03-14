c019a8cd71fe86ec78bcbb48b942fd7e67b8120f
Sparsh Mehta <sparsh.mehta0598@gmail.com>
2022-03-14 12:32:42 +0530

    Commit Changes

---
+++ app.js
@@ -0,0 +1,36 @@
+// *********** select items ************
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+//  edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ********* event listeners ************
+
+// submit form
+form.addEventListener("submit", addItem);
+//  clear list
+clearBtn.addEventListener("click", clearItems);
+// display items onload
+window.addEventListener("DOMContentLoaded", setupItems);
+
+//  ******* functions *************
+
+// add item
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value !== "" && !editFlag) {
+        const element = document.createElement("article");
+        let attr = document.createAttribute("data-id");
+
+    }
+}
