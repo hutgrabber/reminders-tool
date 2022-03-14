0013272288ecb8458a84decc2ed14919a5404f52
Sparsh Mehta <sparsh.mehta0598@gmail.com>
2022-03-14 18:58:00 +0530

    Final Commit (end-project)

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function clearItems() {
 }
 
 // delete items
-function deleteItems() {
+function deleteItem() {
     const element = e.currentTarget.parentElement.parentElement;
     const id = element.dataset.id;
 
@@ -129,5 +129,91 @@ function editItem(e) {
 function setBackToDefault() {
     grocery.value = "";
     editFlag = false;
+    editID = "";
+    submitBtn.textContent = "edit";
+}
 
+//  *********** local storage ************
+
+// add to local storage
+function addToLocalStorage(id, value) {
+    const grocery = { id, value };
+    let items = getLocalStorage();
+    items.push(grocery);
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function getLocalStorage() {
+    return localStorage.getItem("list") ?
+        JSON.parse(localStorage.getItem("list")) : [];
+}
+
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+
+    items = items.filter(function(item) {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+
+    items = items.map(function(items) {
+        if (item.id === id) {
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+//  SETUP LOCALSTORAGE>REMOVEITEM('LIST');
+
+//  ********* setup items *************
+
+function setUpItems() {
+    let items = getLocalStorage();
+
+    if (items.length > 0) {
+        items.forEach(function(item) {
+            createListItem(item.id, item.value);
+        });
+        container.classList.add("show-container");
+    }
+}
+
+function createListItem(id, value) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `
+    <p class="title">${value}</p>
+    <div class="btn-container">
+        <!-- edit btn -->
+        <button type="button" class="edit-btn">
+            <i class="fas fa-edit">
+        </button>
+        <!-- delete btn -->
+        <button type="button" class="delete-btn">
+            <i class="fas fa-trash">
+        </button>
+    </div>
+    `;
+
+    // add event listeners to both buttons
+
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+    list.appendChild(element);
 }
