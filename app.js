f1e760d03d8d9a794cd7921a4ad45b5775d4a84c
hutgrabber <sparsh.mehta0598@gmail.com>
2024-10-30 12:09:32 -0400

    fresh changes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// ****** select items **********
-
 const form = document.querySelector(".grocery-form");
 const alert = document.querySelector(".alert");
 const grocery = document.getElementById("grocery");
@@ -7,7 +5,7 @@ const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
-// edit option
+
 let editElement;
 let editFlag = false;
 let editID = "";
